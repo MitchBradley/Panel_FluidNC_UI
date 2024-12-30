295d4108a12f70d0e78acf5c608bfe9f575ca7eb
Per Lytkemeyer <lytkemeyer@gmail.com>
2024-12-30 19:04:17 +0100

    Update gui.py
    Request updated list of files before displaying file menu

--- src/gui.py
+++ src/gui.py
@@ -322,6 +322,7 @@ def menu_handler(e):
     elif name == 'Jog':
         select_overlay('jog')
     elif name == 'Files':
+        requestFileList()
         select_overlay('files')
     elif name == 'Probe':
         pass
